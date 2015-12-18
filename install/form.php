e2dc4f8bdea3eaf080c11a071f96efc69ce9a138
Arthur Grand <artygrand@gmail.com>
2015-12-18 15:53:11 +0600

    Add installer

---
+++ install/form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SignUp in SyDES</title>
+        <link href="/system/assets/css/signin.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <form action="" method="post">
+            <div class="text">S<span class="red">y</span>DES</div>
+            <?php if (!empty($result['errors'][0])) { ?>
+                <?= $result['errors'][0]; ?>
+            <?php } elseif (!empty($result['errors'][1])) { ?>
+                <?= $result['errors'][1]; ?>
+                <div><button type="submit">Refresh page</button></div>
+            <?php } else { ?>
+                <div><input type="text" name="username" placeholder="Username" required></div>
+                <div><input type="password" name="password" placeholder="Password" required></div>
+                <div><input type="text" name="mastercode" placeholder="Master code" required></div>
+                <div><input type="email" name="email" placeholder="Email" required></div>
+                <div><?= HTML::select('language', App::request()->getPreferredLanguage(array_keys($result['langs'])), $result['langs']); ?></div>
+                <div class="two">&nbsp;</div>
+                <div class="two last"><button type="submit">Create account</button></div>
+                <input type="hidden" id="time_zone" name="time_zone">
+            <?php } ?>
+        </form>
+        <script>
+            document.getElementById('time_zone').value = (new Date()).getTimezoneOffset() / 60;
+        </script>
+    </body>
+</html>
---
+++ install/index.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * SyDES - Lightweight CMF for a simple sites with SQLite database
+ *
+ * @package   SyDES
+ * @copyright 2011-2016, ArtyGrand <artygrand.ru>
+ * @license   GNU GPL v3 or later; see LICENSE
+ */
+require __DIR__.'/../system/config.php';
+require DIR_VENDOR.'/autoload.php';
+
+if (file_exists(DIR_APP.'/config.php')) {
+    die('Delete this folder, pls');
+}
+
+$app = App::instance();
+$app->init();
+
+if ($app->request->is_post && $app->request->has('username')) {
+    $folders = array('cache', 'iblocks', 'languages', 'logs', 'modules', 'plugins', 'sites', 'temp', 'thumbs');
+    foreach ($folders as $folder) {
+        if (!file_exists(DIR_APP.'/'.$folder)) {
+            mkdir(DIR_APP.'/'.$folder, 0777, true);
+        }
+    }
+
+    $post = $app->request->request;
+    if ($post['time_zone'] >= 0) {
+        $post['time_zone'] = '+'.$post['time_zone'];
+    }
+
+    $config = array(
+        'user' => array(
+            'username' => $post['username'],
+            'password' => md5($post['password']),
+            'mastercode' => md5($post['mastercode']),
+            'email' => $post['email'],
+            'autologin' => 0,
+        ),
+        'admin' => array(
+            'time_zone' => 'Etc/GMT'.$post['time_zone'],
+            'date_format' => 'd.m.Y',
+            'check_updates' => 1,
+            'language' => $post['language'],
+            'skin' => 'black',
+            'debug' => 0,
+        ),
+    );
+
+    arr2file($config, DIR_APP.'/config.php');
+
+    $lang_dir = DIR_LANGUAGE.'/'.$post['language'];
+    if (!file_exists($lang_dir)) {
+        mkdir($lang_dir, 0777, true);
+        extractOuterZip($lang_dir, 'http://translate.sydes.ru/download?ext=core&lang='.$post['language']);
+    }
+
+    echo 'installed';
+
+    // залогиниться и ввести мастеркод
+    // $app->response->alert('Delete install folder!');
+    // $app->response->redirect('admin');
+} else {
+    $packages = glob(DIR_LANGUAGE.'/*');
+    $langs = array();
+
+    if (empty($packages)) {
+        $langs = json_decode(getContentByUrl('http://translate.sydes.ru/list'), true);
+    } else {
+        foreach ($packages as $package) {
+            $key = str_replace(DIR_LANGUAGE.'/', '', $package);
+            $arr = include $package.'/translation.php';
+            $langs[$key] = $arr['lang_native_name'];
+        }
+    }
+
+    echo render('form.php', array(
+        'langs' => $langs,
+        'errors' => checkServer()
+    ));
+}
