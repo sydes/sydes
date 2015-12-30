ae65cb459c2da5de2b8afc03f3089559d8fde7c7
Arthur Grand <artygrand@gmail.com>
2015-12-30 23:30:59 +0600

    Added app runner and router

--- system/libraries/App.php
+++ system/libraries/App.php
@@ -7,7 +7,13 @@
  * @copyright 2011-2016, ArtyGrand <artygrand.ru>
  * @license   GNU GPL v3 or later; see LICENSE
  */
-class App extends Pimple\Container {
+namespace App;
+
+use App\Exception\NotFoundHttpException;
+use App\Exception\ForbiddenHttpException;
+use App\Exception\HttpException;
+
+class App extends \Pimple\Container {
 
     private static $instance;
 
@@ -25,11 +31,9 @@ class App extends Pimple\Container {
      *
      */
     public function init() {
-        $this->load = new Loader();
+
 
         // load main config and languages
-
-        $this->cache = new Cache(DIR_CACHE);
 
         // find site by domain
         // load site config
@@ -37,18 +41,86 @@ class App extends Pimple\Container {
         $this['config'] = include DIR_APP.'/config.php';
 
         date_default_timezone_set($this['config']['app']['time_zone']);
-        mb_internal_encoding('UTF-8');
+
+        $site = 's1';
+
+        $this['db'] = function () use ($site) {
+            return new Database($site);
+        };
     }
 
     public function run() {
-        // send Request Through Router
-        //$response = run($request);
-        //throw new BaseException('not good');
-        //strpos();
-        $response = new Response;
-        $response->body = 'hello world '. print_r($response, true);
+        $result = $this->sendRequestThroughRouter($this['request']);
 
-        return $response;
+        if ($result instanceof Http\Response) {
+            return $result;
+        } else {
+            return response($result);
+        }
+    }
+
+    /**
+     * Throw an HttpException with the given data.
+     *
+     * @param int $code
+     * @param string $message
+     * @throws Exception\HttpException
+     */
+    public function abort($code, $message = null) {
+        if ($code == 404) {
+            throw new NotFoundHttpException($message);
+        } elseif ($code == 403) {
+            throw new ForbiddenHttpException($message);
+        }
+        throw new HttpException($code, $message);
+    }
+
+    private function sendRequestThroughRouter($request) {
+        $dispatcher = \FastRoute\cachedDispatcher(function(\FastRoute\RouteCollector $r) {
+            $r->addRoute('GET', '/page/{id:[0-9]+}', 'Test@page');
+            $r->addRoute('GET', '/notfound', 'Test@notfound');
+            $r->addRoute('GET', '/forbidden', 'Test@forbidden');
+            $r->addRoute('GET', '/ajax', 'Test@ajax');
+            $r->addRoute('GET', '/string.txt', 'Test@string');
+            $r->addRoute('GET', '/export', 'Test@export');
+            $r->addRoute('GET', '/html', 'Test@html');
+            $r->addRoute('GET', '/nool', 'Test@nool');
+            $r->addRoute('GET', '/moved', 'Test@moved');
+            $r->addRoute('GET', '/update', 'Test@update');
+            $r->addRoute('GET', '/store', 'Test@store');
+            $r->addRoute('GET', '/ajaxupdate', 'Test@ajaxupdate');
+            $r->addRoute('GET', '/ajaxstore', 'Test@ajaxstore');
+            $r->addRoute('GET', '/', 'Test@index');
+        }, ['cacheFile' => DIR_CACHE.'/route.cache']);
+
+        $routeInfo = $dispatcher->dispatch($request->method, $request->url);
+
+        switch ($routeInfo[0]) {
+            case \FastRoute\Dispatcher::FOUND:
+                $parts = explode('@', $routeInfo[1]);
+                $name = strtolower($parts[0]);
+                $class = $parts[0].'Controller';
+                $method = $parts[1];
+                $vars = $routeInfo[2];
+                break;
+            default:
+                //TODO try to find predefined urls in database
+                if (1){
+                    throw new NotFoundHttpException;
+                }
+                $parts = explode('/', 'test/page/42');
+                $name = $parts[0];
+                $class = ucfirst($parts[0]).'Controller';
+                $method = $parts[1];
+                $vars = array_slice($parts, 2);
+        }
+
+        if (null === ($path = findPath('module', $name))) {
+            throw new \RuntimeException(t('module_folder_not_found'));
+        }
+        include $path;
+        $instance = new $class;
+        return call_user_func_array([$instance, $method], $vars);
     }
 
 }
