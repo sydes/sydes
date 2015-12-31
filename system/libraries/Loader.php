2e2f35c898d441d27814e04621f8de226c36b61b
Arthur Grand <artygrand@gmail.com>
2016-01-01 00:38:03 +0600

    Clear loader

--- system/libraries/Loader.php
+++ system/libraries/Loader.php
@@ -14,7 +14,7 @@ class Loader {
 
     public function model($module) {
         $part = strpos($module, '/') !== false ? explode('/', $module) : [$module, $module];
-        $file = findPath('module', $part[0]).'/model/'.$part[1].'.php';
+        $file = findExt('module', $part[0]).'/model/'.$part[1].'.php';
 
         if (!file_exists($file)) {
             throw new \RuntimeException(sprintf(t('error_file_not_found'), $file));
@@ -35,7 +35,7 @@ class Loader {
         // TODO event before.render.partial with &$template & &$data
 
         $file_override = DIR_THEME.'/'.App::config()->site['theme'].'/module/'.$template.'.php';
-        $file = findPath('module', $part[0]).'/view/'.$part[1].'.php';
+        $file = findExt('module', $part[0]).'/view/'.$part[1].'.php';
         if (file_exists($file_override)) {
             $html = render($file_override, $data);
         } elseif (file_exists($file)) {
@@ -47,33 +47,6 @@ class Loader {
         // TODO event after.render.partial with &$html
 
         return $html;
-    }
-
-    public function language($filename, $global = true, $language = false) {
-        if (!$language) {
-            //$language = App::env()->language;
-        }
-
-        /*
-          $file = DIR_LANGUAGE . $language . '/' . $filename . '.php';
-          if (!is_file($file)){
-          $file = DIR_LANGUAGE . 'en/' . $filename . '.php';
-          if (!is_file($file)){
-          return;
-          }
-          }
-          $array = include $file;
-          if ($global){
-          t('add', $array);
-          } else {
-          return $array;
-          } */
-
-        /*
-          нужно загрузить языковые пакеты, переводы модулей, инфоблоков, фронта, еще чего нибудь.
-          так же нужно загружать как глобально в App::language(), так и локально, для js localization
-
-         */
     }
 
     public function config($model) {
