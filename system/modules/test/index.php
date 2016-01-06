2b49e0d0a909064fd41a6bc191154f69cfcdfd62
Arthur Grand <artygrand@gmail.com>
2016-01-06 14:46:55 +0600

    test to psr

--- system/modules/test/index.php
+++ system/modules/test/index.php
@@ -4,13 +4,16 @@
  * Test module
  */
 
-class TestController {
+class TestController
+{
 
-    public function __construct() {
-        
+    public function __construct()
+    {
+
     }
 
-    public function index() {
+    public function index()
+    {
         return '<a href="page/123">page</a><br>
 <a href="notfound">notfound</a><br>
 <a href="forbidden">forbidden</a><br>
@@ -27,88 +30,107 @@ class TestController {
 <a href="refresh">refresh</a><br>
 <a href="refresh2">refresh and notify</a><br>
 <a href="random">random</a><br>
-<a href="nope">nope</a><br>' . app('contentLang') . ' ' . app('request')->url;
+<a href="nope">nope</a><br>'.app('contentLang').' '.app('request')->url;
     }
 
-    public function page($id) {
+    public function page($id)
+    {
         $d = document();
         $d->data = [
-            'content' => 'some content',
+            'content' => '<p>some content</p>',
             'page_id' => $id,
+            'meta_keywords' => 'key, another key',
+            'title' => 'Page title',
+            'meta_title' => 'Overridden title',
         ];
         $d->addScript('my', '$(document).ready(function(){console.log(\'worked!\')})');
         alert('You\'ve got a message', 'info');
         return $d;
     }
 
-    public function notfound() {
+    public function notfound()
+    {
         abort(404, 'Try Another Castle');
     }
 
-    public function forbidden() {
+    public function forbidden()
+    {
         abort(403);
     }
 
-    public function ajax() {
+    public function ajax()
+    {
         return [
-            'status' => 'ok'
+            'status' => 'ok',
         ];
     }
 
-    public function string() {
+    public function string()
+    {
         $content = 'this is <br> simple string';
         return response($content)->withMime('txt');
     }
 
-    public function export() {
+    public function export()
+    {
         $content = '"id";"title"'."\r\n".'"1";"Test page"';
         return response($content)->withMime('csv')->download('export.'.time().'.csv');
     }
 
-    public function html() {
+    public function html()
+    {
         return '<strong>this is just compiled string</strong>';
     }
 
-    public function nool() {
+    public function nool()
+    {
         // do something
         // if some condition
         return;
         // else do more things
     }
 
-    public function moved() {
+    public function moved()
+    {
         return redirect('page/42');
     }
 
-    public function notifyAfterRedirect() {
+    public function notifyAfterRedirect()
+    {
         notify('Updated');
         return back();
     }
 
-    public function alertAfterRedirect() {
+    public function alertAfterRedirect()
+    {
         alert('This is not stored', 'danger');
         return redirect('/');
     }
 
-    public function ajaxNotify() {
+    public function ajaxNotify()
+    {
         return notify('Not Updated', 'warning');
     }
 
-    public function ajaxAlert() {
+    public function ajaxAlert()
+    {
         return alert('This is stored', 'info');
     }
 
-    public function ajaxRefresh() {
+    public function ajaxRefresh()
+    {
         return refresh();
     }
 
-    public function refreshAndNotify() {
+    public function refreshAndNotify()
+    {
         notify('Notify');
         return refresh();
     }
 
-    public function random() {
-        $rand = rand(0,3);
+    public function random()
+    {
+        $rand = rand(0, 3);
         switch ($rand) {
             case 0:
                 return notify('Notify');
