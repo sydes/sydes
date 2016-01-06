e4f4cf779b49b5b8ba2b3eef1f4538a320d040d2
Arthur Grand <artygrand@gmail.com>
2016-01-06 14:46:07 +0600

    New views

---
+++ system/views/error_common.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>What's wrong?</title>
+    <meta name="robots" content="noindex,nofollow">
+</head>
+<body>
+<h1><?= $code; ?></h1>
+<p><?= $message; ?></p>
+</body>
+</html>
---
+++ system/views/toolbar.php
@@ -0,0 +1,48 @@
+<div id="toolbar">
+    <a href="<?=ADMIN;?>"><?=t('admin');?></a>
+    <div class="btn-group">
+        <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown"><?=t('add');?> <span class="caret"></span></button>
+        <ul class="dropdown-menu dropdown-menu">
+            <?php foreach ($types as $type => $name) { ?>
+                <li><a href="<?=ADMIN ?>/?route=pages/edit&type=<?=$type;?>"><?=$name;?></a></li>
+            <?php } ?>
+        </ul>
+    </div>
+    <a href="<?=ADMIN;?>/?route=common/clearcache&return=<?=$request_uri;?>"><?=t('clear_cache');?></a>
+
+    <span class="divider"></span>
+
+    <?php foreach ($menu as $item) { ?>
+        <?php if ($item['link']) { ?>
+            <a href="<?=$item['link'];?>"><?=$item['title'];?></a>
+        <?php } else { ?>
+            <div class="btn-group">
+                <button type="button" class="btn btn-sm dropdown-toggle" data-toggle="dropdown"><?=$item['title'] ?>
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu dropdown-menu">
+                    <?php foreach ($item['children'] as $child) { ?>
+                        <li><?=$child ?></li>
+                    <?php } ?>
+                </ul>
+            </div>
+        <?php } ?>
+    <?php } ?>
+
+    <a href="<?=ADMIN;?>/?route=user/logout" class="pull-right"><?=t('exit');?></a>
+</div>
+<script>
+    $(document).ready(function () {
+        $('.block-edit').on('click', function () {
+            if ($(this).data('module') == 'iblock') {
+                location.href = '/<?=ADMIN;?>?route=iblocks/edit&iblock=' + $(this).data('item')
+            } else if ($(this).data('module') == 'config') {
+                location.href = '/<?=ADMIN;?>?route=config#' + $(this).data('item')
+            }
+        });
+        $('.block-template').on('click', function () {
+            location.href = '/<?=ADMIN;?>?route=templates/file/edit&tpl=<?=$theme;?>&file=iblock/' +
+                $(this).data('item') + '/' + $(this).data('template') + '.php'
+        });
+        $('body').addClass('with-toolbar');
+    })
+</script>
