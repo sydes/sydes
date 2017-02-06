297d53fd09f9eba9c653d38c41e8581c1dd34c68
Arthur Grand <artygrand@gmail.com>
2017-02-06 22:08:10 +0700

    Renamed constant DIR_LANGUAGE to DIR_L10N
    and reformated code

--- system/config.php
+++ system/config.php
@@ -7,20 +7,20 @@
  * @license   GNU GPL v3 or later; see LICENSE
  */
 
-define('DIR_ROOT',     dirname(__DIR__));
-define('DIR_SYSTEM',   DIR_ROOT.'/system');
-define('DIR_VENDOR',   DIR_ROOT.'/vendor');
-define('DIR_THEME',    DIR_ROOT.'/themes');
-define('DIR_APP',      DIR_ROOT.'/app');
-define('DIR_CACHE',    DIR_APP.'/cache');
-define('DIR_IBLOCK',   DIR_APP.'/iblocks');
-define('DIR_LANGUAGE', DIR_APP.'/languages');
-define('DIR_LOG',      DIR_APP.'/logs');
-define('DIR_MODULE',   DIR_APP.'/modules');
-define('DIR_PLUGIN',   DIR_APP.'/plugins');
-define('DIR_SITE',     DIR_APP.'/sites');
-define('DIR_TEMP',     DIR_APP.'/temp');
-define('DIR_THUMB',    DIR_APP.'/thumbs');
+define('DIR_ROOT', dirname(__DIR__));
+define('DIR_SYSTEM', DIR_ROOT.'/system');
+define('DIR_VENDOR', DIR_ROOT.'/vendor');
+define('DIR_THEME',  DIR_ROOT.'/themes');
+define('DIR_APP',    DIR_ROOT.'/app');
+define('DIR_CACHE',  DIR_APP.'/cache');
+define('DIR_IBLOCK', DIR_APP.'/iblocks');
+define('DIR_L10N',   DIR_APP.'/l10n');
+define('DIR_LOG',    DIR_APP.'/logs');
+define('DIR_MODULE', DIR_APP.'/modules');
+define('DIR_PLUGIN', DIR_APP.'/plugins');
+define('DIR_SITE',   DIR_APP.'/sites');
+define('DIR_TEMP',   DIR_APP.'/temp');
+define('DIR_THUMB',  DIR_APP.'/thumbs');
 
 define('SYDES_VERSION', '3.0.0-a2');
 define('APP_START', microtime(true));
