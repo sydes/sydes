969f0076b6c34feb79c61eb999cae2be850c1b63
Arthur Grand <artygrand@gmail.com>
2017-02-13 19:47:19 +0700

    Changed system config

--- system/config.php
+++ system/config.php
@@ -8,16 +8,16 @@
  */
 
 define('DIR_ROOT', dirname(__DIR__));
-define('DIR_SYSTEM', DIR_ROOT.'/system');
-define('DIR_VENDOR', DIR_ROOT.'/vendor');
-define('DIR_THEME',  DIR_ROOT.'/themes');
 define('DIR_APP',    DIR_ROOT.'/app');
+define('DIR_SYSTEM', DIR_ROOT.'/system');
+define('DIR_THEME',  DIR_ROOT.'/themes');
+define('DIR_UPLOAD', DIR_ROOT.'/upload');
+define('DIR_VENDOR', DIR_ROOT.'/vendor');
 define('DIR_CACHE',  DIR_APP.'/cache');
 define('DIR_IBLOCK', DIR_APP.'/iblocks');
 define('DIR_L10N',   DIR_APP.'/l10n');
 define('DIR_LOG',    DIR_APP.'/logs');
 define('DIR_MODULE', DIR_APP.'/modules');
-define('DIR_PLUGIN', DIR_APP.'/plugins');
 define('DIR_SITE',   DIR_APP.'/sites');
 define('DIR_TEMP',   DIR_APP.'/temp');
 define('DIR_THUMB',  DIR_APP.'/thumbs');
