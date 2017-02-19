c117e451ef3f2f97edce4bd2ea89701748beedd1
Arthur Grand <artygrand@gmail.com>
2017-02-19 21:16:28 +0700

    Added main translations

--- system/modules/Main/languages/en.php
+++ system/modules/Main/languages/en.php
@@ -3,4 +3,21 @@
     'menu_constructors' => 'Constructors',
     'menu_tools' => 'Tools',
     'menu_system' => 'System',
+
+    'yes' => 'Yes',
+    'no' => 'No',
+
+    'added' => 'Added',
+    'saved' => 'Saved',
+    'deleted' => 'Deleted',
+
+    'by' => 'By',
+    'new' => 'New',
+    'add' => 'Add',
+    'edit' => 'Edit',
+    'clone' => 'Clone',
+    'delete' => 'Delete',
+    'activate' => 'Activate',
+    'install' => 'Install',
+    'uninstall' => 'Uninstall',
 );
