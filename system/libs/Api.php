2b3ca21443385f653e1f619d641f13e1aef4084d
Arthur Grand <artygrand@gmail.com>
2017-03-03 14:02:03 +0700

    Rewrited file headers

--- system/libs/Api.php
+++ system/libs/Api.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * SyDES - Lightweight CMF for a simple sites with SQLite database
- *
- * @package   SyDES
+ * @link      https://github.com/sydes/sydes
  * @copyright 2011-2017, ArtyGrand <artygrand.ru>
  * @license   GNU GPL v3 or later; see LICENSE
  */
