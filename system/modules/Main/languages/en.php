8d4f05a38053b53943f7db249ba8ef183c82ae5e
Arthur Grand <artygrand@gmail.com>
2017-04-02 20:20:55 +0700

    Added some base translations

--- system/modules/Main/languages/en.php
+++ system/modules/Main/languages/en.php
@@ -2,17 +2,27 @@
 return array(
     'module_main' => 'Main Module',
 
+    'error_class_not_found' => 'Class {class} not found',
+    'error_method_not_found' => 'Method {method} not found in {class}',
     'error_module_folder_not_found' => 'Folder for module {module} is not found',
+
     'confirm_deletion' => 'Are you really want to delete this?',
 
     'yes' => 'Yes',
     'no' => 'No',
 
+    'or' => 'or',
+    'by' => 'By',
+
     'added' => 'Added',
     'saved' => 'Saved',
     'deleted' => 'Deleted',
+    'uploaded' => 'Uploaded',
+    'installed' => 'Installed',
+    'uninstalled' => 'Uninstalled',
+    'activated' => 'Activated',
+    'deactivated' => 'Deactivated',
 
-    'by' => 'By',
     'new' => 'New',
     'add' => 'Add',
     'edit' => 'Edit',
@@ -22,4 +32,5 @@ return array(
     'install' => 'Install',
     'uninstall' => 'Uninstall',
     'settings' => 'Settings',
+    'upload' => 'Upload',
 );
--- system/modules/Main/languages/ru.php
+++ system/modules/Main/languages/ru.php
@@ -2,17 +2,27 @@
 return array(
     'module_main' => 'Главный модуль',
 
+    'error_class_not_found' => 'Класс {class} не найден',
+    'error_method_not_found' => 'Метод {method} не найден в {class}',
     'error_module_folder_not_found' => 'Директория модуля {module} не найдена',
+
     'confirm_deletion' => 'Вы действительно хотите удалить это?',
 
     'yes' => 'Да',
     'no' => 'Нет',
 
+    'or' => 'или',
+    'by' => 'От',
+
     'added' => 'Добавлено',
     'saved' => 'Сохранено',
     'deleted' => 'Удалено',
+    'uploaded' => 'Загружено',
+    'installed' => 'Установлено',
+    'uninstalled' => 'Разустановлено',
+    'activated' => 'Активировано',
+    'deactivated' => 'Деактивировано',
 
-    'by' => 'От',
     'new' => 'Новый',
     'add' => 'Добавить',
     'edit' => 'Изменить',
@@ -22,4 +32,5 @@ return array(
     'install' => 'Установить',
     'uninstall' => 'Удалить',
     'settings' => 'Настройки',
+    'upload' => 'Загрузить',
 );
