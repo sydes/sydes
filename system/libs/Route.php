1c64fc32addf42ab131fe4dae490b3dcd447426c
Arthur Grand <artygrand@gmail.com>
2017-04-24 16:07:37 +0700

    Added route resource

--- system/libs/Route.php
+++ system/libs/Route.php
@@ -8,5 +8,20 @@ namespace Sydes;
 
 class Route extends \FastRoute\RouteCollector
 {
-
+    /**
+     * This single route declaration creates multiple routes to handle a variety of actions on the resource
+     *
+     * @param string $alias
+     * @param string $module
+     * @param string $itemRegex
+     */
+    public function resource($alias, $module, $itemRegex = '\d+')
+    {
+        $this->get('/admin/'.$alias, $module.'@index');
+        $this->get('/admin/'.$alias.'/create', $module.'@create');
+        $this->post('/admin/'.$alias, $module.'@store');
+        $this->get('/admin/'.$alias.'/{id:'.$itemRegex.'}', $module.'@edit');
+        $this->put('/admin/'.$alias.'/{id:'.$itemRegex.'}', $module.'@update');
+        $this->delete('/admin/'.$alias.'/{id:'.$itemRegex.'}', $module.'@destroy');
+    }
 }
