a9439bdbe601b70ce2963b440daee718990e0288
Arthur Grand <artygrand@gmail.com>
2017-05-05 23:05:41 +0700

    Model WIP

--- system/libs/Model.php
+++ system/libs/Model.php
@@ -6,13 +6,70 @@
  */
 namespace Sydes;
 
-class Model
+abstract class Model implements \ArrayAccess
 {
-    /** @var \PDO */
-    protected $db;
+    protected $connection = 'site';
+    protected $table;
+    protected $primaryKey = 'id';
+    protected $attributes = [];
+    protected $fillable = [];
 
-    public function __construct($db)
+    public function __construct(array $attrs = [])
     {
-        $this->db = $db;
+        $this->fill($attrs);
+    }
+
+    public function fill(array $attrs)
+    {
+        foreach ($this->fillableFromArray($attrs) as $key => $value) {
+            $this->attributes[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    protected function fillableFromArray(array $attrs)
+    {
+        if (count($this->fillable) > 0) {
+            return array_intersect_key($attrs, array_flip($this->fillable));
+        }
+
+        return $attrs;
+    }
+
+    public function offsetExists($key)
+    {
+        return !empty($this->attributes[$key]);
+    }
+
+    public function offsetGet($key)
+    {
+        return $this->attributes[$key];
+    }
+
+    public function offsetSet($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+
+    public function offsetUnset($key)
+    {
+        $this->attributes[$key] = '';
+    }
+
+    public function toArray()
+    {
+        return $this->attributes;
+    }
+
+    public function getTable()
+    {
+        if ($this->table) {
+            return $this->table;
+        } else {
+            $name = get_class($this);
+
+            return snake_case(($pos = strrpos($name, '\\')) ? substr($name, $pos + 1) : $name).'s';
+        }
     }
 }
