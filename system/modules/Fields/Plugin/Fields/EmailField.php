f3c11eae858d7593f2b83925509604eaa732f22c
Arthur Grand <artygrand@gmail.com>
2017-05-08 17:00:48 +0700

    Added field Email

---
+++ system/modules/Fields/Plugin/Fields/EmailField.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link      https://github.com/sydes/sydes
+ * @copyright 2011-2017, ArtyGrand <artygrand.ru>
+ * @license   GNU GPL v3 or later; see LICENSE
+ */
+namespace Module\Fields\Plugin\Fields;
+
+class EmailField extends FieldBase
+{
+    public function getField()
+    {
+        return \H::emailInput($this->name, $this->value, $this->settings);
+    }
+}
