bc75b759b73e9701e274d7589bbcfdf12ab4b81c
Arthur Grand <artygrand@gmail.com>
2017-06-27 19:42:01 +0700

    Add new field type - hidden

---
+++ system/modules/Entity/Plugins/Fields/HiddenField.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link      https://github.com/sydes/sydes
+ * @copyright 2011-2017, ArtyGrand <artygrand.ru>
+ * @license   GNU GPL v3 or later; see LICENSE
+ */
+namespace Module\Entity\Plugins\Fields;
+
+use Module\Entity\Models\Field;
+
+class HiddenField extends Field
+{
+    public function formInput()
+    {
+        return '';
+    }
+}
