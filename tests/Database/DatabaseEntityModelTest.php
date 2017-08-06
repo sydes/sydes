136606c6699a281c521a429fc6dc653cb941b20b
Arthur Grand <artygrand@gmail.com>
2017-08-06 18:13:32 +0700

    Added first tests

---
+++ tests/Database/DatabaseEntityModelTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Sydes\Tests\Database;
+
+use PHPUnit\Framework\TestCase;
+use Sydes\Database\Entity\Model;
+
+/**
+ * @covers Model
+ */
+final class DatabaseEntityModelTest extends TestCase
+{
+    public function testCanBeCreatedWithStructure()
+    {
+        $model = Model::unserialize([
+            'table' => 'stub',
+            'fields' => [
+                'title' => [
+                    'type' => 'Text'
+                ]
+            ]
+        ]);
+
+        $this->assertInstanceOf(Model::class, $model);
+
+        $this->assertEquals('stub', $model->getTable());
+
+        $this->assertEquals('id', $model->getKeyName());
+        $this->assertTrue($model->hasIncrementing());
+
+        $this->assertTrue(isset($model->title));
+
+        $model = Model::unserialize([
+            'fields' => [
+                'code' => [
+                    'primary' => true,
+                    'type' => 'Text'
+                ]
+            ]
+        ]);
+
+        $this->assertEquals('models', $model->getTable());
+
+        $this->assertEquals('code', $model->getKeyName());
+        $this->assertFalse($model->hasIncrementing());
+
+        $this->assertEquals([
+            'table' => 'models',
+            'fields' => [
+                'code' => [
+                    'primary' => true,
+                    'type' => 'Text'
+                ]
+            ]
+        ], $model->serialize());
+    }
+}
