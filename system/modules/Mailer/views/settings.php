af5c8efca31940c5cf45d361ca1f94fe57a38f22
Arthur Grand <artygrand@gmail.com>
2017-08-07 11:52:00 +0700

    Changed mailer settings view

--- system/modules/Mailer/views/settings.php
+++ system/modules/Mailer/views/settings.php
@@ -16,39 +16,38 @@
                     'label' => 'mailer_default_to',
                 ]);?>
             </div>
-            <div class="col-sm-6">
-                <?=Form::input('use_smtp', 'YesNo', [
-                    'label' => 'mailer_smtp',
-                ]);?>
-            </div>
-        </div>
-
-        <div class="row on-smtp">
-            <div class="col-sm-6">
-                <?=Form::input('smtp_host', 'Text', [
-                    'label' => 'mailer_smtp_host',
-                ]);?>
-            </div>
-            <div class="col-sm-6">
-                <?=Form::input('smtp_port', 'Text', [
-                    'label' => 'mailer_smtp_port',
-                ]);?>
-            </div>
-            <div class="col-sm-6">
-                <?=Form::input('smtp_user', 'Text', [
-                    'label' => 'mailer_smtp_user',
-                ]);?>
-            </div>
-            <div class="col-sm-6">
-                <?=Form::input('smtp_password', 'Text', [
-                    'label' => 'mailer_smtp_password',
-                ]);?>
-            </div>
         </div>
 
         <?=Form::input('send_also', 'Text', [
             'label' => 'mailer_send_also',
         ]);?>
+
+        <?=Form::input('use_smtp', 'YesNo', [
+            'label' => 'mailer_smtp',
+        ]);?>
+
+        <div class="row on-smtp">
+            <div class="col-sm-3">
+                <?=Form::input('smtp_host', 'Text', [
+                    'label' => 'mailer_smtp_host',
+                ]);?>
+            </div>
+            <div class="col-sm-3">
+                <?=Form::input('smtp_port', 'Text', [
+                    'label' => 'mailer_smtp_port',
+                ]);?>
+            </div>
+            <div class="col-sm-3">
+                <?=Form::input('smtp_user', 'Text', [
+                    'label' => 'mailer_smtp_user',
+                ]);?>
+            </div>
+            <div class="col-sm-3">
+                <?=Form::input('smtp_password', 'Text', [
+                    'label' => 'mailer_smtp_password',
+                ]);?>
+            </div>
+        </div>
 
         <?=Form::close();?>
     </div>
