62f55e4ce1a3d9b42f46d6879f5f7592bd800ca4
Arthur Grand <artygrand@gmail.com>
2017-08-08 11:44:19 +0700

    Use Silly instead

--- system/modules/Main/routes/console.php
+++ system/modules/Main/routes/console.php
@@ -1,178 +1,153 @@
 <?php
-/**
- * @link      https://github.com/sydes/sydes
- * @copyright 2011-2017, ArtyGrand <artygrand.ru>
- * @license   GNU GPL v3 or later; see LICENSE
- */
-namespace Module\Main;
+if (!$app) {
+    return;
+}
 
-use Sydes\Console\Commands;
-use Sydes\Console\Output;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
-// TODO переписать под silly
-class Cli
-{
-    public function __construct(Commands $commands, Output $you)
-    {
-        $commands->add('help', function () use ($commands, $you) {
-            $you->say('Welcome to SyDES CLI')
-                ->say('')
-                ->say('Usage:')
-                ->say('  command [arguments] [options]')
-                ->say('')
-                ->say('Options:')
-                ->say('  -h, --help  Display description about command')
-                ->say('')
-                ->say('Available commands:')
-                ->say('  '.implode("\n  ", $commands->getCommands()));
-        });
+$app->command('site id', function ($id, OutputInterface $output) {
+    if (!is_int($id) || !file_exists(app('dir.site').'/'.$id)) {
+        $output->writeln("Can't find site with id ".$id);
+        return;
+    }
 
-        $commands->add('goto-site id', function ($id) use ($you) {
-            if (!is_int($id) || !file_exists(app('dir.site').'/'.$id)) {
-                $you->say("Can't find site with id ".$id);
+    $_SESSION['site'] = $id;
+
+    $output->writeln('You in site '.$id);
+})->descriptions('Select site to work with', [
+    'id' => 'Site ID',
+]);
+
+$app->command('install [extension] [name] [-d|--demo]',
+    function ($extension, $name, $demo, InputInterface $input, OutputInterface $output) {
+        if ($extension && $name) {
+            if ($extension == 'module') {
+                model('Modules')->install($name);
+            } else {
+                model('Themes')->activate($name);
+            }
+
+            $output->writeln('You installed '.$extension.' '.$name);
+        } else {
+            if (model('Settings/App')->isCreated()) {
+                $output->writeln('Site already installed');
                 return;
             }
 
-            $_SESSION['site'] = $id;
+            /** @var QuestionHelper $helper */
+            $helper = $this->getHelperSet()->get('question');
+            $params = [
+                'email'    => 'admin@domain.tld',
+                'username' => 'demo',
+                'password' => 'demo',
+                'siteName' => 'Demo site',
+                'locale'   => 'en',
+                'domain'   => '',
+                'timeZone' => 0,
+            ];
 
-            $you->say('You in site '.$id);
-        }, ['Select site to work with', [
-            'id' => 'Site ID',
-        ]]);
+            if ($demo) {
 
-        $commands->add('install [extension] [name] [-d|--demo]',
-            function ($extension, $name, $demo = false) use ($you) {
-                if ($extension && $name) {
-                    if ($extension == 'module') {
-                        model('Modules')->install($name);
-                    } else {
-                        model('Themes')->activate($name);
-                    }
-
-                    $you->say('You installed '.$extension.' '.$name);
-                } else {
-                    if (model('Settings/App')->isCreated()) {
-                        $you->say('Site already installed');
-                        return;
-                    }
-
-                    $params = [
-                        'email' => 'admin@domain.tld',
-                        'username' => 'demo',
-                        'password' => 'demo',
-                        'siteName' => 'Demo site',
-                        'locale' => 'en',
-                        'domain' => '',
-                        'timeZone' => 0,
-                    ];
-
-                    if ($demo) {
-
-                        $you->say("You can enter with these credentials:
+                $output->writeln("You can enter with these credentials:
 Username: demo\nPassword: demo\nDeveloper password: demo\nBut we need some info...");
 
-                    } else {
+            } else {
 
-                        $you->say('Ok! Just answer the following questions')
-                            ->say('First, create your account');
-                        $params['email'] = $you->ask('Email');
-                        $you->rewind();
-                        $params['username'] = $you->ask('Username');
-                        $you->rewind();
-                        $params['password'] = $you->ask('Password');
-                        $you->rewind();
-                        $you->say('Good!');
-                        $you->say("Now we'll create your site");
-                        $params['siteName'] = mb_convert_encoding($you->ask('Site name'), 'UTF-8', 'cp866');
-                        $you->rewind();
-                        $params['locale'] = $you->ask('Locale (en)');
-                        $you->rewind();
+                $output->writeln('Ok! Just answer the following questions');
+                $output->writeln('First, create your account');
 
-                    }
+                $params['email'] = $helper->ask($input, $output, new Question('Email'));
+                $params['username'] = $helper->ask($input, $output, new Question('Username'));
+                $params['password'] = $helper->ask($input, $output, new Question('Password'));
 
-                    $params['domain'] = $you->ask('Domain (test.com)');
-                    $you->rewind();
+                $output->writeln('Good!');
+                $output->writeln("Now we'll create your site");
+                $params['siteName'] = mb_convert_encoding(
+                    $helper->ask($input, $output, new Question('Site name')), 'UTF-8', 'cp866'
+                );
+                $params['locale'] = $helper->ask($input, $output, new Question('Locale (en)'));
 
-                    $installer = model('Main/Installer');
-                    $installer->step1();
-                    $installer->step2($params['locale']);
-                    $installer->step3($params);
-
-                    $what = $demo ? 'Demo site' : 'Site';
-                    $you->say($what.' installed')
-                        ->say('Have a nice day!');
-                }
-            }, ['Install site or provide type and name of extension to install it', [
-                'extension' => "Optional, 'module' or 'theme'",
-                'name' => 'Optional, name of extension',
-                '-d, --demo' => 'Use to install demo site',
-            ]]);
-
-        $commands->add('update [extension] [name]', function ($extension = null, $name = null) use ($you) {
-            if (model('Updater')->up($extension, $name)) {
-                if ($extension && $name) {
-                    $you->say('You updated '.$extension.' '.$name);
-                } else {
-                    $you->say('SyDES updated');
-                }
             }
-        }, ['Update site or provide type and name of extension to update it', [
-            'extension' => "Optional, 'module' or 'theme'",
-            'name' => 'Optional, name of extension',
-        ]]);
 
-        $commands->add('uninstall [extension] [name] [-d]',
-            function ($extension = false, $name = false, $d = false) use ($commands, $you) {
-                if ($extension && $name) {
-                    if ($extension == 'module') {
-                        model('Modules')->uninstall($name);
-                        $you->say('You uninstalled module '.$name);
-                    } else {
-                        $you->say('You can\'t uninstall theme');
-                    }
+            $params['domain'] = $helper->ask($input, $output, new Question('Domain (test.com)'));
 
-                    if ($d) {
-                        $commands->run('delete', [$extension, $name]);
-                    }
-                } else {
-                    model('Main/Installer')->uninstall();
-                    $you->say('SyDES uninstalled');
-                }
-            }, ['Uninstall site or provide type and name of extension to uninstall it', [
-                'extension' => "Optional, 'module' or 'theme'",
-                'name' => 'Optional, name of extension',
-                '-d' => 'Delete extension after uninstalling',
-            ]]);
+            $installer = model('Main/Installer');
+            $installer->step1();
+            $installer->step2($params['locale']);
+            $installer->step3($params);
 
-        $commands->add('download extension name [-i]', function ($extension, $name, $i = false) use ($commands, $you) {
+            $what = $demo ? 'Demo site' : 'Site';
+            $output->writeln($what.' installed');
+            $output->writeln('Have a nice day!');
+        }
+    })->descriptions('Install site or provide type and name of extension to install it', [
+    'extension'  => "Optional, 'module' or 'theme'",
+    'name'       => 'Optional, name of extension',
+    '--demo' => 'Use to install demo site',
+]);
 
-            $you->say('Downloading...');
-
-            $you->say('You downloaded '.$extension.' '.$name);
-
-            if ($i) {
-                $commands->run('install', [$extension, $name]);
-            }
-        }, ['Download extension by type and name', [
-            'extension' => "'module' or 'theme'",
-            'name' => 'name of extension',
-            '-i' => 'Install extension after downloading',
-        ]]);
-
-        $commands->add('delete extension name', function ($extension, $name) use ($you) {
-
-            $you->say('You deleted '.$extension.' '.$name);
-        }, ['Delete extension by type and name', [
-            'extension' => "'module' or 'theme'",
-            'name' => 'name of extension',
-        ]]);
-
-        $commands->add('make extension name', function ($extension, $name) use ($you) {
-
-            $you->say('You created base files for '.$extension.' '.$name);
-        }, ['Make base files for extension', [
-            'extension' => "'module', 'plugin', 'iblock', or 'theme'",
-            'name' => 'name of extension',
-        ]]);
+$app->command('update [extension] [name]', function ($extension = null, $name = null, OutputInterface $output) {
+    if (model('Updater')->up($extension, $name)) {
+        if ($extension && $name) {
+            $output->writeln('You updated '.$extension.' '.$name);
+        } else {
+            $output->writeln('SyDES updated');
+        }
     }
-}
+})->descriptions('Update site or provide type and name of extension to update it', [
+    'extension' => "Optional, 'module' or 'theme'",
+    'name'      => 'Optional, name of extension',
+]);
+
+$app->command('uninstall [extension] [name] [-d|--delete]',
+    function ($extension = false, $name = false, $delete, OutputInterface $output) {
+        if ($extension && $name) {
+            if ($extension == 'module') {
+                model('Modules')->uninstall($name);
+                $output->writeln('You uninstalled module '.$name);
+            } else {
+                $output->writeln('You can\'t uninstall theme');
+            }
+
+            if ($delete) {
+                $this->runCommand("delete {$extension} {$name}", $output);
+            }
+        } else {
+            model('Main/Installer')->uninstall();
+            $output->writeln('SyDES uninstalled');
+        }
+    })->descriptions('Uninstall site or provide type and name of extension to uninstall it', [
+    'extension' => "Optional, 'module' or 'theme'",
+    'name'      => 'Optional, name of extension',
+    '--delete'  => 'Delete extension after uninstalling',
+]);
+
+$app->command('download extension name [-i|--install]', function ($extension, $name, $install, OutputInterface $output) {
+    $output->writeln('Downloading...');
+    $output->writeln('You downloaded '.$extension.' '.$name);
+
+    if ($install) {
+        $this->runCommand("install {$extension} {$name}", $output);
+    }
+})->descriptions('Download extension by type and name', [
+    'extension' => 'module or theme',
+    'name'      => 'name of extension',
+    '--install' => 'Install extension after downloading',
+]);
+
+$app->command('delete extension name', function ($extension, $name, OutputInterface $output) {
+    $output->writeln('You deleted '.$extension.' '.$name);
+})->descriptions('Delete extension by type and name', [
+    'extension' => "'module' or 'theme'",
+    'name'      => 'name of extension',
+]);
+
+$app->command('make extension name', function ($extension, $name, OutputInterface $output) {
+    $output->writeln('You created base files for '.$extension.' '.$name);
+})->descriptions('Make base files for extension', [
+    'extension' => 'module, plugin, iblock or theme',
+    'name'      => 'name of extension',
+]);
